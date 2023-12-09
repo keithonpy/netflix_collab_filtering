5cbc48b8b5bbf22913bdd3d4b668bcc3ae1c6661
keithonpy <pokpok9301@gmail.com>
2023-12-09 14:07:28 +0000

    update the EM algorithm

---
+++ em.py
@@ -0,0 +1,123 @@
+"""Mixture model for matrix completion"""
+from typing import Tuple
+import numpy as np
+from scipy.special import logsumexp
+from common import GaussianMixture
+
+
+def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
+    """E-step: Softly assigns each datapoint to a gaussian component
+
+    Args:
+        X: (n, d) array holding the data, with incomplete entries (set to 0)
+        mixture: the current gaussian mixture
+
+    Returns:
+        np.ndarray: (n, K) array holding the soft counts
+            for all components for all examples
+        float: log-likelihood of the assignment
+
+    """
+    n, _ = X.shape
+    K, _ = mixture.mu.shape
+    post = np.zeros((n, K))
+
+    ll = 0
+    for i in range(n):
+        for j in range(K):
+            likelihood = gaussian(X[i], mixture.mu[j], mixture.var[j])
+            post[i, j] = mixture.p[j] * likelihood
+        total = post[i, :].sum()
+        post[i, :] = post[i, :] / total
+        ll += np.log(total)
+
+    return post, ll
+
+
+def gaussian(x: np.ndarray, mean: np.ndarray, var: float) -> float:
+    """Computes the probablity of vector x under a normal distribution
+
+    Args:
+        x: (d, ) array holding the vector's coordinates
+        mean: (d, ) mean of the gaussian
+        var: variance of the gaussian
+
+    Returns:
+        float: the probability
+    """
+    d = len(x)
+    log_prob = -d / 2.0 * np.log(2 * np.pi * var)
+    log_prob -= 0.5 * ((x - mean)**2).sum() / var
+    return np.exp(log_prob)
+
+
+def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
+          min_variance: float = .25) -> GaussianMixture:
+    """M-step: Updates the gaussian mixture by maximizing the log-likelihood
+    of the weighted dataset
+
+    Args:
+        X: (n, d) array holding the data, with incomplete entries (set to 0)
+        post: (n, K) array holding the soft counts
+            for all components for all examples
+        mixture: the current gaussian mixture
+        min_variance: the minimum variance for each gaussian
+
+    Returns:
+        GaussianMixture: the new gaussian mixture
+    """
+    n, d = X.shape
+    _, K = post.shape
+
+    n_hat = post.sum(axis=0)
+    p = n_hat / n
+
+    mu = np.zeros((K, d))
+    var = np.zeros(K)
+
+    for j in range(K):
+        # Computing mean
+        mu[j, :] = (X * post[:, j, None]).sum(axis=0) / n_hat[j]
+        # Computing variance
+        sse = ((mu[j] - X)**2).sum(axis=1) @ post[:, j]
+        var[j] = sse / (d * n_hat[j])
+
+    return GaussianMixture(mu, var, p)
+
+
+def run(X: np.ndarray, mixture: GaussianMixture,
+        post: np.ndarray) -> Tuple[GaussianMixture, np.ndarray, float]:
+    """Runs the mixture model
+
+    Args:
+        X: (n, d) array holding the data
+        post: (n, K) array holding the soft counts
+            for all components for all examples
+
+    Returns:
+        GaussianMixture: the new gaussian mixture
+        np.ndarray: (n, K) array holding the soft counts
+            for all components for all examples
+        float: log-likelihood of the current assignment
+    """
+    prev_ll = None
+    ll = None
+    while (prev_ll is None or ll - prev_ll > 1e-6 * np.abs(ll)):
+        prev_ll = ll
+        post, ll = estep(X, mixture)
+        mixture = mstep(X, post, mixture)
+
+    return mixture, post, ll
+
+
+def fill_matrix(X: np.ndarray, mixture: GaussianMixture) -> np.ndarray:
+    """Fills an incomplete matrix according to a mixture model
+
+    Args:
+        X: (n, d) array of incomplete data (incomplete entries =0)
+        mixture: a mixture of gaussians
+
+    Returns
+        np.ndarray: a (n, d) array with completed data
+    """
+    raise NotImplementedError
