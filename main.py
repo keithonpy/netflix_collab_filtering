7e908a9af6b1a72bd70d19d5beb0fe6fc2c7e263
keithonpy <pokpok9301@gmail.com>
2023-12-09 14:28:52 +0000

    test for em algorithm

--- main.py
+++ main.py
@@ -23,6 +23,25 @@ for k in range(1, 5):
 	mixture, post = common.init(X, k, best_seed)
 	mixture, post, cost = kmeans.run(X, mixture, post)
 
-	title = f"K-means | K = {k}| seed = {seed} | cost = {cost}"
+	title = f"K-means | K = {k}| seed = {best_seed} | cost = {cost}"
+	print(title)
+	common.plot(X, mixture, post, title)
+
+################ EM algo ####################
+for k in range(1, 5):
+	max_ll = -float("inf")
+	best_seed = 0
+	for seed in range(5):
+		mixture, post = common.init(X, k, seed)
+		mixture, post, ll = em.run(X, mixture, post)
+		# find the best seed that give maximum log-likelihood
+		if ll > max_ll:
+			max_ll = ll
+			best_seed = seed
+
+	mixture, post = common.init(X, k, best_seed)
+	mixture, post, ll = em.run(X, mixture, post)
+
+	title = f"EM algo | K = {k}| seed = {best_seed} | ll = {ll}"
 	print(title)
 	common.plot(X, mixture, post, title)
